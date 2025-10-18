86002beeef6a616caadbae2bf2be12584ed54513
Tim Hawkins <56164332+illmade-knight@users.noreply.github.com>
2025-10-18 13:13:06 +0100

    feat(messenger): add chat-ui

---
+++ libs/messenger/chat-ui/src/lib/chat-ui/chat-ui.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-chat-ui',
+  imports: [],
+  templateUrl: './chat-ui.html',
+  styleUrl: './chat-ui.css',
+})
+export class ChatUi {}
